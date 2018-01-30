ebde2901292a7da2e7c7389f7556d61e9e4527f2
214169 <harpalsgh@gmail.com>
2018-01-30 16:03:34 -0700

    Service-Discovery

--- applications/movie-fun-app/src/main/java/org/superbiz/moviefun/DevelopmentSecurityConfig.java
+++ applications/movie-fun-app/src/main/java/org/superbiz/moviefun/DevelopmentSecurityConfig.java
@@ -1,6 +1,7 @@
 package org.superbiz.moviefun;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestOperations;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 public class DevelopmentSecurityConfig {
 
     @Bean
+    @LoadBalanced
     public RestOperations restOperations() {
         return new RestTemplate();
     }
